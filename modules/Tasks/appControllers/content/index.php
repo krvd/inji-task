1c5e1aa6afd4594adbaee9f829e0439d612f00ce
Alexey Krupsky <admin@inji.ru>
2016-01-29 12:34:14 +0700

    prefilter

--- modules/Tasks/appControllers/content/index.php
+++ modules/Tasks/appControllers/content/index.php
@@ -1,8 +1,14 @@
 <div class="row">
   <div class="col-lg-12">
     <?php
-    $categoryManager = new Ui\DataManager('Tasks\Task', 'manager', []);
-    $categoryManager->draw();
+    $categoryManager = new Ui\DataManager('Tasks\Task', 'manager');
+    $categoryManager->draw([
+        'filters' => [
+            'task_status_id' => [
+                'value' => [1,2]
+            ]
+        ]
+    ]);
     ?>
   </div>
 </div>
