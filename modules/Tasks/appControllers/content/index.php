35eb64fccf25270a07dd0116f3bf4b6eeec7434b
Alexey Krupsky <admin@inji.ru>
2016-01-29 11:40:43 +0700

    add menu, hide projects

--- modules/Tasks/appControllers/content/index.php
+++ modules/Tasks/appControllers/content/index.php
@@ -1,11 +1,5 @@
 <div class="row">
-  <div class="col-lg-4">
-    <?php
-    $categoryManager = new Ui\DataManager('Tasks\Project', 'manager', []);
-    $categoryManager->draw();
-    ?>
-  </div>
-  <div class="col-lg-8">
+  <div class="col-lg-12">
     <?php
     $categoryManager = new Ui\DataManager('Tasks\Task', 'manager', []);
     $categoryManager->draw();
