556c1e02a6f37853ed0b591c34e1219f0b933a24
Daniil Pokusaev <acckoekrevedko@gmail.com>
2016-01-29 13:23:00 +0700

    fix

--- modules/Tasks/appControllers/content/index.php
+++ modules/Tasks/appControllers/content/index.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-lg-12">
     <?php
-    $categoryManager = new Ui\DataManager('Tasks\Task', 'manager', []);
+    $categoryManager = new Ui\DataManager('Tasks\Task', 'manager');
     $categoryManager->draw();
     ?>
   </div>
